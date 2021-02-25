8d742011ae2beb7ef8e37f1a3260c6b0cd24a51d
Gorila <lasvacations@yandex.ru>
2021-02-26 00:19:25 +0300

    adding function about processor temperature

--- percentile.py
+++ percentile.py
@@ -4,3 +4,16 @@ import json
 import numpy as np
 summ = []
 start_time = time.time()
+
+def cpuStat ():
+    count = 0
+    while True:
+        data = psutil.cpu_percent(1)
+        time.sleep(1)
+        print(data)
+        count += 1
+        summ.append(data)
+        if count == 10:
+            break
+
+cpuStat()
