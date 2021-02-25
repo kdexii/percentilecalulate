f3cc569eca175ad6f277a1b713fb8b4bc16650a3
Gorila <lasvacations@yandex.ru>
2021-02-26 00:49:47 +0300

    add perc count summary

--- percentile.py
+++ percentile.py
@@ -2,18 +2,29 @@ import psutil
 import time
 import json
 import numpy as np
+from tqdm import tqdm
 summ = []
-start_time = time.time()
-
+start_time = float(time.time())
 def cpuStat ():
     count = 0
     while True:
-        data = psutil.cpu_percent(1)
+        data = psutil.cpu_percent(0)
         time.sleep(1)
-        print(data)
+        print('temperature processor:',data, '℃')
         count += 1
         summ.append(data)
-        if count == 10:
+        
+        if count == 3:
             break
-
+def percCountSumm ():
+    print('enter the percentile value:')
+    percCount = input()
+    percCount = float(percCount)
+    perc = np.percentile(summ, percCount)
+    perc = int(perc)
+    print('summ percentile value :  ',perc)
 cpuStat()
+percCountSumm()
+print('converted float:', summ,  
+      ',to type:', type(summ).__name__)
+print("the program was completed in %s seconds\n" % (time.time() - start_time))
